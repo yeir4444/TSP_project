4faf37c26a8aef4b7857adaaf1a5f64fd07b0bd5
SaraAlameen82 <60101453@udst.edu.qa>
2025-04-01 11:40:05 +0300

    Update main.py to run the visualization code

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
-import numpy as np
-import pandas as pd
-from algorthims.aco import AntColony
-from algorthims.ga import GeneticAlgorithm
+from algorithms.aco import AntColony
+from algorithms.ga import GeneticAlgorithm
 from utils.tsp_utils import load_distance_matrix
 from utils.tsp_utils import compute_tour_length
+from utils.visualization import visualize_solution
 
 # Load distance matrix
 distances = load_distance_matrix("data/city_distances.csv")
@@ -19,3 +18,6 @@ ga = GeneticAlgorithm(distances)
 best_ga_path, best_ga_length = ga.run()
 best_ga_tour_length = compute_tour_length(best_ga_path, distances)
 print(f"GA Best Path: {best_ga_path}, Distance: {best_ga_tour_length}")
+
+# Visualize the solutions
+visualize_solution(best_aco_path, best_ga_path, distances)
