dc6262728d011fb68cd58042ff92c70f9b33ae0d
SaraAlameen82 <60101453@udst.edu.qa>
2025-04-05 11:49:29 +0300

    Update tsp_utils.py

--- src/utils/tsp_utils.py
+++ src/utils/tsp_utils.py
@@ -1,9 +1,26 @@
 import pandas as pd
 
 def load_distance_matrix(filepath):
+    """
+    {
+        "summary": "Loads the distance matrix from a CSV file.",
+        "parameters": {
+            "filepath": "str - Path to the CSV file containing the distance matrix."
+        },
+        "returns": "np.array - 2D distance matrix."
+    }
+    """
     return pd.read_csv(filepath).to_numpy()
 
 def compute_tour_length(tour, distances):
+    """
+    {
+        "summary": "Computes the total distance of a complete TSP tour.",
+        "parameters": {
+            "tour": "list - Ordered list of cities representing the tour.",
+            "distances": "np.array - Distance matrix between cities."
+        },
+        "returns": "float - Total length of the tour."
+    }
+    """
     return sum(distances[tour[i], tour[i+1]] for i in range(len(tour)-1)) + distances[tour[-1], tour[0]]
-
-
